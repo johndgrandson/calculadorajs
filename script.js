f56eb8d11615a92f912017621dda67a15477006b
johndgrandson <joaogcnmg@gmail.com>
2022-11-04 19:37:19 -0300

    projeto calculadora

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function insert(n) {
+    var num = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = num + n;
+}
+
+function clean() {
+    document.getElementById('resultado').innerHTML ='';
+}
+
+function back() {
+    var res = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = res.substring(0, res.length -1);
+}
+
+function cal() {
+    var res = document.getElementById('resultado').innerHTML;
+    if(res) {
+        document.getElementById('resultado').innerHTML = eval(res)
+    }else {
+        window.alert('nada para calcular')
+    }
+}
